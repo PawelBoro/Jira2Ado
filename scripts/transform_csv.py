44882cb45915f594d64eeedabbd274d848a50fa5
Pawel Borowiak <pawelborowiak.eng@gmail.com>
2023-08-28 22:20:45 +0200

    Fix

--- scripts/transform_csv.py
+++ scripts/transform_csv.py
@@ -5,25 +5,15 @@ Script synopsis.
 scripts/transform_csv.py
 """
 
-import datetime
 import json
 
 import pandas as pd
 
 
 # Defining functions
-def add_rows_to_dataframe(dataframe, json_file_path):
-    """
-    Add rows to a DataFrame from a JSON file.
-    Parameters:
-        dataframe (pd.DataFrame): The original DataFrame.
-        json_file_path (str): The path to the JSON file containing
-        data for new rows.
-    Returns:
-        pd.DataFrame: The updated DataFrame with the new rows added at the top.
-    """
+def add_rows_to_dataframe(dataframe, jsonfilepath):
     # Read JSON data from the file
-    with open(json_file_path, "r") as json_file:
+    with open(jsonfilepath, "r") as json_file:
         json_data = json.load(json_file)
 
     # Create a new DataFrame with the specified JSON data
@@ -51,153 +41,6 @@ def delete_rows_with_empty_work_item_type(dataframe):
     return filtered_dataframe
 
 
-def process_dataframe(dataframe):
-    new_rows = []
-    rows_to_remove = []
-    previous_title_3 = None
-
-    for index, row in dataframe.iterrows():
-        title_3 = row["Title 3"]
-
-        if title_3 != previous_title_3:
-            # Create a new row with the desired values
-            new_row = {
-                "ID": "",
-                "Work Item Type": "Feature",
-                "Title 1": "",
-                "Title 2": "",
-                "Title 3": title_3,
-                "Title 4": "",
-                "Assigned To": "Szymon Osiecki",
-                "State": "Active",
-                "Iteration Path": "\\Cloud General Sprints",
-                "Area Path": "\\AzureCSE\\SolutionEngineering",
-                "Description": "",
-                "Acceptance Criteria": "",
-                "PG Business Value": "AI Factory Project delivery",
-                "Value Area": "Business",
-                "Work Category": "",
-                "Tags": "Agility",
-                "Start Date": "",
-                "Resolved Date": "",
-            }
-            new_rows.append(new_row)
-
-        # Remove the title from the original row
-        row["Title 3"] = ""
-        rows_to_remove.append(index)
-
-        previous_title_3 = title_3
-
-    # Create a DataFrame from the new_rows list
-    new_rows_df = pd.DataFrame(new_rows)
-
-    # Concatenate the new rows to the original DataFrame
-    dataframe = pd.concat([dataframe, new_rows_df], ignore_index=True)
-
-    # Remove the rows with empty "Title 3"
-    dataframe = dataframe.drop(rows_to_remove)
-
-    return dataframe
-
-    # Create an empty DataFrame to store the new rows
-    new_rows_df = pd.DataFrame(columns=dataframe.columns)
-
-    # Get the unique values in the "Title 3" column
-    unique_titles = dataframe["Title 3"].unique()
-
-    for title in unique_titles:
-        # Find the index of the first occurrence of the title
-        first_occurrence_index = dataframe[dataframe["Title 3"] == title].index[0]
-
-        # Create a new row with the desired values
-        new_row = {
-            "ID": "",
-            "Work Item Type": "Feature",
-            "Title 1": "Agility via Product Management",
-            "Title 2": "",
-            "Title 3": title,
-            "Title 4": "",
-            "Assigned To": "Szymon Osiecki",
-            "State": "Active",
-            "Iteration Path": "\\Cloud General Sprints",
-            "Area Path": "\\AzureCSE\\SolutionEngineering",
-            "Description": "",
-            "Acceptance Criteria": "",
-            "PG Business Value": "AI Factory Project delivery",
-            "Value Area": "Business",
-            "Work Category": "",
-            "Tags": "Agility",
-            "Start Date": "",
-            "Resolved Date": "",
-        }
-
-        # Concatenate the new row with the empty DataFrame
-        new_rows_df = pd.concat(
-            [new_rows_df, pd.DataFrame([new_row])], ignore_index=True
-        )
-
-        # Add the new row above the first occurrence of the title
-        dataframe = pd.concat(
-            [
-                dataframe.iloc[:first_occurrence_index],
-                new_rows_df,
-                dataframe.iloc[first_occurrence_index:],
-            ],
-            ignore_index=True,
-        )
-
-    return dataframe
-
-
-def add_row_above_first_occurrence(dataframe):
-    # Create an empty DataFrame to store the new rows
-    new_rows = []
-
-    # Get the unique values in the "Title 3" column
-    unique_titles = dataframe["Title 3"].unique()
-
-    for title in unique_titles:
-        # Find all occurrences of the title
-        occurrence_indices = dataframe[dataframe["Title 3"] == title].index
-
-        # Create a new row with the desired values
-        new_row = {
-            "ID": "",
-            "Work Item Type": "Feature",
-            "Title 1": "",
-            "Title 2": "",
-            "Title 3": title,
-            "Title 4": "",
-            "Assigned To": "Szymon Osiecki",
-            "State": "Active",
-            "Iteration Path": "\\Cloud General Sprints",
-            "Area Path": "\\AzureCSE\\SolutionEngineering",
-            "Description": "",
-            "Acceptance Criteria": "",
-            "PG Business Value": "AI Factory Project delivery",
-            "Value Area": "Business",
-            "Work Category": "",
-            "Tags": "EE-ProductEnhancement",
-            "Start Date": "",
-            "Resolved Date": "",
-        }
-
-        # Append the new row to the list
-        new_rows.append(new_row)
-
-        # Remove all occurrences of the value from the "Title 3" column
-        dataframe.loc[occurrence_indices, "Title 3"] = ""
-
-    # Create a DataFrame from the new_rows list
-    new_rows_df = pd.DataFrame(new_rows)
-
-    # Concatenate the new rows with the original DataFrame
-    dataframe = pd.concat([new_rows_df, dataframe], ignore_index=True)
-
-    return dataframe
-
-
 def create_feature_rows(dataframe):
     # Create an empty DataFrame to store the new rows
     new_rows_df = pd.DataFrame(columns=dataframe.columns)
@@ -207,6 +50,7 @@ def create_feature_rows(dataframe):
 
     for index, row in dataframe.iterrows():
         title_3 = row["Title 3"]
+
         if title_3 != current_title:
             # Create a new "Feature" row with the same "Title 3" value
             new_feature_row = {
@@ -234,10 +78,6 @@ def create_feature_rows(dataframe):
             # Update the current_title
             current_title = title_3
 
-        #     # Delete the value from "Title 3" for "User story" rows
-        # if row["Work Item Type"] == "User story":
-        #     new_rows_df.at[index, "Title 3"] = ""
-
         # Append the original row
         new_rows_df = new_rows_df.append(row, ignore_index=True)
 
@@ -249,6 +89,24 @@ def delete_title3_for_user_stories(dataframe):
     for index, row in dataframe.iterrows():
         if row["Work Item Type"] == "User story":
             dataframe.at[index, "Title 3"] = ""
+    return dataframe
+
+
+def change_names(dataframe):
+    # Define a mapping of name changes
+    name_mapping = {
+        "Pawel Borowiak": "Borowiak, Pawel",
+        "kamil gajek": "Kamil Gajek",
+        "maciej znalezniak": "Maciej Znalezniak",
+    }
+
+    # Apply the name changes to the "Assigned To" column
+    dataframe["Assigned To"] = (
+        dataframe["Assigned To"].map(name_mapping).fillna(dataframe["Assigned To"])
+    )
+
+    # Return the updated DataFrame
+    return dataframe
 
 
 # Path to the csv file generated from Jira
@@ -259,7 +117,7 @@ df = pd.read_csv(file_path)
 print(df)
 
 # Path to the JSON file containing new rows
-json_file_path = "standard_rows.json"
+jsonfilepath = "standard_rows.json"
 
 # Get the current date
 current_date = pd.Timestamp.now()
@@ -292,7 +150,12 @@ new_df = pd.DataFrame(columns=new_columns)
 new_df["Work Item Type"] = df["Issue Type"].apply(
     lambda x: "User story" if x == "Story" else ""
 )
-new_df["Title 3"] = "[" + df["Project key"] + "] " + df["Project name"]
+new_df["Title 3"] = df.apply(
+    lambda row: "[" + str(row["Project key"]) + "] " + str(row["Epic Link Summary"])
+    if row["Project key"] in ["ITS", "ITSMS"]
+    else "[" + str(row["Project key"]) + "] " + str(row["Project name"]),
+    axis=1,
+)
 new_df["Title 4"] = df["Summary"]
 new_df["Assigned To"] = df["Assignee"]
 new_df["State"] = "New"
@@ -307,22 +170,30 @@ new_df["Tags"] = "EE-ProductEnhancement"
 new_df["Start Date"] = df["Created"]
 new_df["Resolved Date"] = current_date + pd.DateOffset(months=3)
 
+# Sort 'new_df' by the 'Title 3' column
+new_df.sort_values(by="Title 3", inplace=True)
+
+# Reset the index if needed
+new_df.reset_index(drop=True, inplace=True)
+
 # Filtering the data
 filtered_df = delete_rows_with_empty_work_item_type(new_df)
 
 # Processing DataFrame
-new_dataframe = add_row_above_first_occurrence(filtered_df)
 create_fea_rows = create_feature_rows(filtered_df)
 delete_df = delete_title3_for_user_stories(create_fea_rows)
 
 # Call the function to add new rows from the JSON file
-newest_df = add_rows_to_dataframe(create_fea_rows, json_file_path)
+newest_df = add_rows_to_dataframe(create_fea_rows, jsonfilepath)
+
+# Change the names for ADO
+new_names_df = change_names(newest_df)
 
 # Display the updated DataFrame
 print(newest_df)
 
 # Save 'new_df' to a CSV file
 newest_df.to_csv("docs/newest_df.csv", index=False)
-new_dataframe.to_csv("docs/new_dataframe.csv", index=False)
+delete_df.to_csv("docs/delete_df.csv", index=False)
 filtered_df.to_csv("docs/filtered_df.csv", index=False)
 create_fea_rows.to_csv("docs/create_feature_rows.csv", index=False)
