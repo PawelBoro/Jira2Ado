a2908ff203b6a7965df0d82875a840024eedeadc
Pawel Borowiak <pawelborowiak.eng@gmail.com>
2023-08-29 20:08:39 +0200

    Fix: Final code

--- scripts/transform_csv.py
+++ scripts/transform_csv.py
@@ -26,15 +26,6 @@ def add_rows_to_dataframe(dataframe, jsonfilepath):
 
 
 def delete_rows_with_empty_work_item_type(dataframe):
-    """
-    Delete rows where the 'Work Item Type' column is empty.
-
-    Parameters:
-        dataframe (pd.DataFrame): The DataFrame to modify.
-
-    Returns:
-        pd.DataFrame: The DataFrame with empty 'Work Item Type' rows removed.
-    """
     # Use boolean indexing to filter rows where 'Work Item Type' is not empty
     filtered_dataframe = dataframe[dataframe["Work Item Type"] == "User story"]
 
@@ -61,7 +52,7 @@ def create_feature_rows(dataframe):
                 "Title 3": title_3,
                 "Title 4": "",
                 "Assigned To": "Szymon Osiecki",
-                "State": "Active",
+                "State": "New",
                 "Iteration Path": "\\Cloud General Sprints",
                 "Area Path": "\\AzureCSE\\SolutionEngineering",
                 "Description": "",
@@ -98,6 +89,7 @@ def change_names(dataframe):
         "Pawel Borowiak": "Borowiak, Pawel",
         "kamil gajek": "Kamil Gajek",
         "maciej znalezniak": "Maciej Znalezniak",
+        "Aleksander Kamczyc": "Kamczyc, Aleksander Non-PG",
     }
 
     # Apply the name changes to the "Assigned To" column
@@ -109,12 +101,38 @@ def change_names(dataframe):
     return dataframe
 
 
+def change_status(dataframe):
+    # Define a mapping of name changes
+    status_mapping = {
+        "Done": "Closed",
+        "To Do": "New",
+        "Review": "Under Review",
+        "Blocked": "On Hold",
+    }
+
+    # Apply the name changes to the "Status 2" column
+    dataframe["State 2"] = (
+        dataframe["State 2"].map(status_mapping).fillna(dataframe["State 2"])
+    )
+
+    # Set "Status 2" to "Active" when "Work Item Type" is "Feature"
+    dataframe.loc[dataframe["Work Item Type"] == "Feature", "State 2"] = "Active"
+
+    # Return the updated DataFrame
+    return dataframe
+
+
+# Function to update "Acceptance Criteria" for empty values
+def update_acceptance_criteria(dataframe):
+    dataframe["Acceptance Criteria"].fillna("User story completed", inplace=True)
+    return dataframe
+
+
 # Path to the csv file generated from Jira
 file_path = "docs/get_multiple_projects_jira_records_to_csv.csv"
 
 # Create a Data Frame
 df = pd.read_csv(file_path)
-print(df)
 
 # Path to the JSON file containing new rows
 jsonfilepath = "standard_rows.json"
@@ -159,7 +177,7 @@ new_df["Title 3"] = df.apply(
 new_df["Title 4"] = df["Summary"]
 new_df["Assigned To"] = df["Assignee"]
 new_df["State"] = "New"
-new_df["Iteration Path"] = "\Cloud General Sprints"
+new_df["Iteration Path"] = "one-portfolio\CSE\CSE Solution Engineering\August'23"
 new_df["Area Path"] = "\AzureCSE\SolutionEngineering"
 new_df["Description"] = df["Description"]
 new_df["Acceptance Criteria"] = df["Custom field (Acceptance Criteria)"]
@@ -169,6 +187,7 @@ new_df["Work Category"] = "Business Request"
 new_df["Tags"] = "EE-ProductEnhancement"
 new_df["Start Date"] = df["Created"]
 new_df["Resolved Date"] = current_date + pd.DateOffset(months=3)
+new_df["State 2"] = df["Status"]
 
 # Sort 'new_df' by the 'Title 3' column
 new_df.sort_values(by="Title 3", inplace=True)
@@ -179,21 +198,31 @@ new_df.reset_index(drop=True, inplace=True)
 # Filtering the data
 filtered_df = delete_rows_with_empty_work_item_type(new_df)
 
+# Update "Acceptance Criteria" for empty values
+updated_df = update_acceptance_criteria(filtered_df)
+
 # Processing DataFrame
-create_fea_rows = create_feature_rows(filtered_df)
+create_fea_rows = create_feature_rows(updated_df)
 delete_df = delete_title3_for_user_stories(create_fea_rows)
 
 # Call the function to add new rows from the JSON file
 newest_df = add_rows_to_dataframe(create_fea_rows, jsonfilepath)
 
 # Change the names for ADO
-new_names_df = change_names(newest_df)
+change_names(newest_df)
 
-# Display the updated DataFrame
-print(newest_df)
+# Create a copy of df
+last_df = newest_df.copy()
 
-# Save 'new_df' to a CSV file
-newest_df.to_csv("docs/newest_df.csv", index=False)
-delete_df.to_csv("docs/delete_df.csv", index=False)
-filtered_df.to_csv("docs/filtered_df.csv", index=False)
-create_fea_rows.to_csv("docs/create_feature_rows.csv", index=False)
+# Save 'newest_df' to a CSV file without the "State 2" column
+newest_df.drop(columns=["State 2"], inplace=True)
+newest_df.to_csv("docs/first_df.csv", index=False)
+
+# Change the status
+change_status(last_df)
+
+# Create a copy of 'newest_df' and change "State" to "State 2" in the copy
+last_df["State"] = last_df["State 2"]
+last_df.drop(columns=["State 2"], inplace=True)
+# Save the copy to a CSV file
+last_df.to_csv("docs/last_df.csv", index=False)
